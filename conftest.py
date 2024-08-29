58ccf59de1bd11677d2ac0ea52991a3fa0dc245b
Azamat-V-U <tester_01_980@mail.ru>
2024-08-29 11:48:13 +0300

    modified chrome_driver

--- conftest.py
+++ conftest.py
@@ -11,10 +11,13 @@ from pages.sale_page import SalePage
 def driver():
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     chrome_driver = webdriver.Chrome(options=options)
     chrome_driver.maximize_window()
     chrome_driver.implicitly_wait(3)
     yield chrome_driver
+    # chrome_driver.save_screenshot(f"{str(random.randint(100, 10000))}.png")
 
 
 @pytest.fixture()
